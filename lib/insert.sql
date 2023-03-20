33accb867ba0a7899a877e4cb4768517ff0703b7
minyoungshin <mins6649@gmail.com>
2023-03-20 17:03:51 -0400

    sqlite queries

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 20, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Black", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Brown", "Anxious", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 17, "M", "Brown", "Excitable", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "Panda", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "Brown", "Grumpy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "Brown", "Sociable", 1);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (0, "F", "Brown", "Excitable", 1);
+
+
+
+
